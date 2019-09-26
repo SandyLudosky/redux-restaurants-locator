0360bd95715a28b6fd111b6684103d235c506c6c
Sandy Ludosky <sandyludosky@gmail.com>
2019-09-27 00:21:55 +0200

    react-switch - show restaurants open

--- app/components/scenes/RestaurantsList/components/ResultsComponent.tsx
+++ app/components/scenes/RestaurantsList/components/ResultsComponent.tsx
@@ -1,8 +1,8 @@
   
 import React from "react";
-import { FlatList, Text, View, StyleSheet } from 'react-native'
+import { FlatList, Text, View, StyleSheet, Switch } from 'react-native'
 import { IRestaurant } from "../../../../models/restaurant";
-import { data } from '../../../../config/data'
+import { VisibilityFilters } from '../../../../lib/actions'
 
 const Item = (restaurant: IRestaurant) => {
     const { name, id } = restaurant
@@ -11,15 +11,39 @@ const Item = (restaurant: IRestaurant) => {
         <Text key={id} style={styles.title}>{name}</Text>
       </View>)
 }
-  
-const ResultsComponent = ({ restaurants }: any) => {
-  return(<FlatList
-    data={restaurants}
-    renderItem={({ item }) => (<Item {...item}/>)}
-    keyExtractor={(item: IRestaurant) => item.id}/>)
+
+const ResultsComponent = ({ restaurants, open, onSwitch }: any) => {
+
+  return(
+  <View style={{flex: 1, flexDirection: 'column'}}>
+    <View style={styles.switch}>
+        <Text style={styles.text}>open now : </Text>
+        <Switch onValueChange={(e) => onSwitch(e)} 
+                value={open}
+                trackColor={{ false: '#ccc', true: '#16a085'}} />
+    </View>
+    <FlatList
+      data={restaurants}
+      renderItem={({ item }) => (<Item {...item}/>)}
+      keyExtractor={(item: IRestaurant) => item.id}/>
+  </View>)
 }
 
 const styles = StyleSheet.create({
+    switch: {
+       marginTop: 20,
+       marginBottom: 20, 
+       marginRight: 20,
+
+       flexDirection: 'row',
+       justifyContent: 'flex-end'
+    },
+    text: {
+      fontSize: 18, 
+      fontWeight: '600',
+      paddingTop: 5,
+      color: '#333'
+    },
     item: {
       backgroundColor: '#ecf0f1',
       padding: 20,
--- app/components/scenes/RestaurantsList/containers/ResultsList.tsx
+++ app/components/scenes/RestaurantsList/containers/ResultsList.tsx
@@ -1,25 +1,38 @@
 import { connect } from 'react-redux'
 import { setVisibilityFilter } from '../../../../lib/actions'
 import ResultsComponent from '../components/ResultsComponent'
-import { IRestaurant } from '../../../../models/restaurant'
-import { data } from '../../../../config/data'
 import { VisibilityFilters } from '../../../../lib/actions'
 
+/*
+Data from store
+*/
 const getVisibleRestaurants = (restaurants: any[], filter: string) => {
   switch (filter) {
     case VisibilityFilters.SHOW_ALL: return restaurants
-    case VisibilityFilters.SHOW_OPEN: return restaurants.filter(t => t.opening_hours.open_now)
+    case VisibilityFilters.SHOW_OPEN: 
+    return restaurants.filter(restaurant => {
+        return restaurant.opening_hours ? restaurant.opening_hours.open_now : false
+    })
     default: throw new Error('Unknown filter: ' + filter)
   }
 }
+const getFilter = (filter: string): boolean  => {
+  const { SHOW_OPEN } = VisibilityFilters
+  return filter == SHOW_OPEN ? true : false
+}
 const mapStateToProps = (state: any) => {
   return {
-    restaurants: getVisibleRestaurants(state.restaurants, state.visibilityFilter)
+    restaurants: getVisibleRestaurants(state.restaurants, state.visibilityFilter),
+    open: getFilter(state.visibilityFilter)
   }
 }
 const mapDispatchToProps = (dispatch: any) => {
+  const { SHOW_ALL, SHOW_OPEN } = VisibilityFilters
   return {
-    onSwitch:(filter:string) => { dispatch(setVisibilityFilter(filter)) }
+    onSwitch:(e:boolean) => { 
+      console.log(e)
+      dispatch(setVisibilityFilter( e ? SHOW_OPEN : SHOW_ALL)) 
+    }
   }
 }
 
